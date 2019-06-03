d2cb124cab6fd9c2fa170054e5ff25a66f6394fe
Jananie-wathsala <jana.kum@hotmail.com>
2019-06-04 00:41:26 +0530

    Added laravel api for backend

---
+++ laraApi/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Retrieve all users
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(){
+       $users=User::all();
+       return response()->json($users,200);
+    }
+    public function show($id){
+        $user=User::find($id);
+        return response()->json($user,200);
+    }
+
+}
