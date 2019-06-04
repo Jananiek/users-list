f071fee75201533d49ec103bb81be5367b82f1d4
Jananie-wathsala <jana.kum@hotmail.com>
2019-06-04 11:10:24 +0530

    Added API calls

--- laraApi/app/User.php
+++ laraApi/app/User.php
@@ -2,9 +2,19 @@
 
 namespace App;
 
+use App\Video;
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $fillable=['fname','lname'];
+    protected $fillable = ['fname', 'lname'];
+
+    /**
+     * all videos for one user
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function videos()
+    {
+        return $this->hasMany('App\Video', 'user_id');
+    }
 }
--- laraApi/database/migrations/2019_06_04_053327_create_videos_table.php
+++ laraApi/database/migrations/2019_06_04_053327_create_videos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateVideosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('videos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('fname',50);
-            $table->string('lname',50);
+            $table->integer('user_id');
+            $table->string('title');
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('videos');
     }
 }
